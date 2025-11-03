035ec9ebda73d7aa689e030913b7e00110ac8e53
Helen306-us <sevillahelen345@gmail.com>
2025-11-03 16:11:54 -0600

    cambiando la vista de categoria index.php

--- index.php
+++ index.php
@@ -178,71 +178,58 @@
 	<!-- end features Area -->
 
 	<!-- Start category Area -->
+
 	<section class="category-area">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-lg-8 col-md-12">
-					<div class="row">
-						<div class="col-lg-8 col-md-8">
-							<div class="single-deal">
-								<div class="overlay"></div>
-								<img class="img-fluid w-100" src="img/category/c1.jpg" alt="">
-								<a href="img/category/c1.jpg" class="img-pop-up" target="_blank">
-									<div class="deal-details">
-										<h6 class="deal-title">Reloj de lujo</h6>
-									</div>
-								</a>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4">
-							<div class="single-deal">
-								<div class="overlay"></div>
-								<img class="img-fluid w-100" src="img/category/c2.jpg" alt="">
-								<a href="img/category/c2.jpg" class="img-pop-up" target="_blank">
-									<div class="deal-details">
-										<h6 class="deal-title">Reloj casual</h6>
-									</div>
-								</a>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4">
-							<div class="single-deal">
-								<div class="overlay"></div>
-								<img class="img-fluid w-100" src="img/category/c3.jpg" alt="">
-								<a href="img/category/c3.jpg" class="img-pop-up" target="_blank">
-									<div class="deal-details">
-										<h6 class="deal-title">Reloj para parejas</h6>
-									</div>
-								</a>
-							</div>
-						</div>
-						<div class="col-lg-8 col-md-8">
-							<div class="single-deal">
-								<div class="overlay"></div>
-								<img class="img-fluid w-100" src="img/category/c4.jpg" alt="">
-								<a href="img/category/c4.jpg" class="img-pop-up" target="_blank">
-									<div class="deal-details">
-										<h6 class="deal-title">Reloj exclusivo</h6>
-									</div>
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6">
-					<div class="single-deal">
-						<div class="overlay"></div>
-						<img class="img-fluid w-100" src="img/category/c5.jpg" alt="">
-						<a href="img/category/c5.jpg" class="img-pop-up" target="_blank">
-							<div class="deal-details">
-								<h6 class="deal-title">Ofertas nuevas todos los dias</h6>
-							</div>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+  <div class="container">
+    <div class="row justify-content-center">
+      <!-- Primer bloque de 4 categorías -->
+      <div class="col-lg-6 col-md-6 mb-4">
+        <div class="single-deal position-relative">
+          <img class="img-fluid w-100 rounded" src="img/category/CC4.png" alt="Cadenas">
+          <div class="deal-details position-absolute top-50 start-50 translate-middle text-center text-white">
+            <h2 class="text-white">CADENAS</h2>
+            <p>Descubre diseños elegantes en oro que realzan tu estilo con delicadeza y distinción.</p>
+            <a href="#" class="btn btn-outline-light mt-2">Ver más</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6 col-md-6 mb-4">
+        <div class="single-deal position-relative">
+          <img class="img-fluid w-100 rounded" src="img/category/CP3.png" alt="Aretes">
+          <div class="deal-details position-absolute top-50 start-50 translate-middle text-center text-white">
+            <h2 class="text-white">ARETES</h2>
+            <p>Aretes únicos que resaltan tu belleza y complementan tu look con sofisticación.</p>
+            <a href="#" class="btn btn-outline-light mt-2">Ver más</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6 col-md-6 mb-4">
+        <div class="single-deal position-relative">
+          <img class="img-fluid w-100 rounded" src="img/category/CA2.png" alt="Anillos">
+          <div class="deal-details position-absolute top-50 start-50 translate-middle text-center text-white">
+            <h2 class="text-white">ANILLOS</h2>
+            <p>Descubre nuestras joyas atemporales de amor, poder y estilo personal.</p>
+            <a href="#" class="btn btn-outline-light mt-2">Ver más</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6 col-md-6 mb-4">
+        <div class="single-deal position-relative">
+          <img class="img-fluid w-100 rounded" src="img/category/CB1.png" alt="Brazaletes">
+          <div class="deal-details position-absolute top-50 start-50 translate-middle text-center text-white">
+            <h2 class="text-white">BRAZALETES</h2>
+            <p>Brazaletes creados para acompañarte y destacar en cada movimiento.</p>
+            <a href="#" class="btn btn-outline-light mt-2">Ver más</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+	
 	<!-- End category Area -->
 
 	<!-- start product Area -->
