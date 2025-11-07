d0fdc76d7dd2b15f1471015efbd73a1d171356cd
Helen Urrutia <sevillahelen345@gmail.com>
2025-11-06 22:45:49 -0600

    Probando cambios en el index

--- index.php
+++ index.php
@@ -55,11 +55,10 @@
 							<li class="nav-item submenu dropdown">
 							<a class="nav-link" href="cart.php">Cart</a></li>
 							</li>
-							<!--
+
 							<li class="nav-item submenu dropdown">
 							<a class="nav-link" href="404.php">404 Page</a></li>
 							</li>
-							-->
 
 							<li class="nav-item submenu dropdown">
 							<a class="nav-link" href="about.php">Sobre Nosotros</a></li>
@@ -78,9 +77,7 @@
 							<a class="nav-link" href="Shop.php">Productos</a></li>
 							</li>
 
-							<li class="nav-item submenu dropdown">
-							<a class="nav-link" href="login.php">Iniciar sesion</a></li>
-							</li>
+							
 						</ul>
 					</div>
 				</div>
@@ -105,7 +102,7 @@
 							</div>
 							<div class="col-lg-7">
 								<div class="banner-img">
-									<img class="img-fluid" src="img/banner/casio.webp" alt="">
+									<img class="img-fluid" src="img/banner/Anillo.png" alt="">
 								</div>
 							</div>
 						</div>
@@ -119,7 +116,7 @@
 							</div>
 							<div class="col-lg-7">
 								<div class="banner-img">
-									<img class="img-fluid" src="img/banner/banner-img.png" alt="">
+									<img class="img-fluid" src="img/banner/banner-img2.png" alt="">
 								</div>
 							</div>
 						</div>
@@ -181,74 +178,60 @@
 	<!-- end features Area -->
 
 	<!-- Start category Area -->
-	<section class="category-area">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-lg-8 col-md-12">
-					<div class="row">
-						<div class="col-lg-8 col-md-8">
-							<div class="single-deal">
-								<div class="overlay"></div>
-								<img class="img-fluid w-100" src="img/category/c1.jpg" alt="">
-								<a href="img/category/c1.jpg" class="img-pop-up" target="_blank">
-									<div class="deal-details">
-										<h6 class="deal-title">Reloj de lujo</h6>
-									</div>
-								</a>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4">
-							<div class="single-deal">
-								<div class="overlay"></div>
-								<img class="img-fluid w-100" src="img/category/c2.jpg" alt="">
-								<a href="img/category/c2.jpg" class="img-pop-up" target="_blank">
-									<div class="deal-details">
-										<h6 class="deal-title">Reloj casual</h6>
-									</div>
-								</a>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4">
-							<div class="single-deal">
-								<div class="overlay"></div>
-								<img class="img-fluid w-100" src="img/category/c3.jpg" alt="">
-								<a href="img/category/c3.jpg" class="img-pop-up" target="_blank">
-									<div class="deal-details">
-										<h6 class="deal-title">Reloj para parejas</h6>
-									</div>
-								</a>
-							</div>
-						</div>
-						<div class="col-lg-8 col-md-8">
-							<div class="single-deal">
-								<div class="overlay"></div>
-								<img class="img-fluid w-100" src="img/category/c4.jpg" alt="">
-								<a href="img/category/c4.jpg" class="img-pop-up" target="_blank">
-									<div class="deal-details">
-										<h6 class="deal-title">Reloj exclusivo</h6>
-									</div>
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6">
-					<div class="single-deal">
-						<div class="overlay"></div>
-						<img class="img-fluid w-100" src="img/category/c5.jpg" alt="">
-						<a href="img/category/c5.jpg" class="img-pop-up" target="_blank">
-							<div class="deal-details">
-								<h6 class="deal-title">Ofertas nuevas todos los dias</h6>
-							</div>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+<section class="category-area">
+  <div class="container">
+    <div class="row justify-content-center">
+      <!-- Primer bloque de 4 categorías -->
+      <div class="col-lg-6 col-md-6 mb-4">
+        <div class="single-deal position-relative">
+          <img class="img-fluid w-100 rounded" src="img/category/CC4.png" alt="Cadenas">
+          <div class="deal-details position-absolute top-50 start-50 translate-middle text-center text-white">
+            <h2 class="text-white">CADENAS</h2>
+            <p>Descubre diseños elegantes en oro que realzan tu estilo con delicadeza y distinción.</p>
+            <a href="categoria.php?id=4" class="btn btn-outline-light mt-2">Ver más</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6 col-md-6 mb-4">
+        <div class="single-deal position-relative">
+          <img class="img-fluid w-100 rounded" src="img/category/CP3.png" alt="Aretes">
+          <div class="deal-details position-absolute top-50 start-50 translate-middle text-center text-white">
+            <h2 class="text-white">ARETES</h2>
+            <p>Aretes únicos que resaltan tu belleza y complementan tu look con sofisticación.</p>
+            <a href="categoria.php?id=2" class="btn btn-outline-light mt-2">Ver más</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6 col-md-6 mb-4">
+        <div class="single-deal position-relative">
+          <img class="img-fluid w-100 rounded" src="img/category/CA2.png" alt="Anillos">
+          <div class="deal-details position-absolute top-50 start-50 translate-middle text-center text-white">
+            <h2 class="text-white">ANILLOS</h2>
+            <p>Descubre nuestras joyas atemporales de amor, poder y estilo personal.</p>
+            <a href="categoria.php?id=1" class="btn btn-outline-light mt-2">Ver más</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6 col-md-6 mb-4">
+        <div class="single-deal position-relative">
+          <img class="img-fluid w-100 rounded" src="img/category/CB1.png" alt="Brazaletes">
+          <div class="deal-details position-absolute top-50 start-50 translate-middle text-center text-white">
+            <h2 class="text-white">BRAZALETES</h2>
+            <p>Brazaletes creados para acompañarte y destacar en cada movimiento.</p>
+            <a href="categoria.php?id=3" class="btn btn-outline-light mt-2">Ver más</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 	<!-- End category Area -->
 
-	<!-- start product Area -->
+	<!-- start product Area tambien esta area -->
 	<section class="">
 		<!-- single product slide -->
 		<div class="single-product-slider">
@@ -311,7 +294,7 @@
 					<div class="active-exclusive-product-slider">
 						<!-- single exclusive carousel -->
 						<div class="single-exclusive-slider">
-							<img class="img-fluid" src="img/11.jpg" alt="">
+							<img class="img-fluid" src="img/Pulsera.png" alt="">
 							<div class="product-details">
 								<div class="price">
 									<h6>$150.00</h6>
@@ -341,56 +324,7 @@
 	<!-- Modal -->
 	<div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
 			<div class="modal-dialog">
-				<div class="modal-content">
-				<div class="modal-header">
-					<h5 class="modal-title" id="successModalLabel">Éxito</h5>
-					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-				</div>
-				<div class="modal-body">
-					¡Agregado correctamente!
-				</div>
-				<div class="modal-footer">
-
-				</div>
-				</div>
-			</div>
-        </div>
-	
-
-	<!-- start footer Area -->
-	<footer class="footer-area section_gap">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-3  col-md-6 col-sm-6">
-					<div class="single-footer-widget">
-						<h6>Mas Informacion:</h6>
-						<p>
-						“Descubre la elegancia y el lujo con nuestra exclusiva colección de joyería de las mejores marcas. Cada pieza está diseñada para resaltar tu belleza y estilo, ofreciendo calidad y sofisticación en cada detalle. Tu destino definitivo para joyas que marcan la diferencia en cada ocasión.”
-						</p>
-					</div>
-					<div>
-				<p class="footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-					Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved</p>
-				</div>
-		
-			</div>
-		</div>
-	</footer>
-	<!-- End footer Area -->
-	<script src="js/script.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-	<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
-	<script src="js/vendor/jquery-2.2.4.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
-	 crossorigin="anonymous"></script>
-	<script src="js/vendor/bootstrap.min.js"></script>
-	<script src="js/jquery.ajaxchimp.min.js"></script>
-	<script src="js/jquery.nice-select.min.js"></script>
-	<script src="js/jquery.sticky.js"></script>
-	<script src="js/nouislider.min.js"></script>
-	<script src="js/countdown.js"></script>
-	<script src="js/jquery.magnific-popup.min.js"></script>
-	<script src="js/owl.carousel.min.js"></script>
+				
 	<!--gmaps Js-->
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
 	<script src="js/gmaps.min.js"></script>
