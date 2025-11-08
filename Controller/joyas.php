774b10ab64dde8278ce41e55d62f0ec3f7fe18d8
Helen Urrutia <sevillahelen345@gmail.com>
2025-11-08 13:05:19 -0600

    Agregando soporte CORS en joyas.php

--- Controller/joyas.php
+++ Controller/joyas.php
@@ -1,4 +1,14 @@
 <?php
+// ==== HABILITAR CORS ====
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Manejo de preflight (solicitudes OPTIONS)
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 include '../Database/conexion.php';
 $pdo = new Conexion();
 
