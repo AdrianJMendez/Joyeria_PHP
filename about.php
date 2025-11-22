21a0db357b2a2889f947793c65eb5f5ea85b0714
JackA2104 <aguilarjonathan2004@gmail.com>
2025-11-21 20:24:49 -0600

    nada

--- about.php
+++ about.php
@@ -15,7 +15,7 @@
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
 	<!-- fontawesome -->
-	<link rel="stylesheet" href="assets/css/all.min.css">
+	<!-- <link rel="stylesheet" href="assets/css/all.min.css"> -->
 	<!-- bootstrap -->
 	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
 	<!-- owl carousel -->
@@ -35,7 +35,7 @@
 		CSS
 		============================================= -->
 	<link rel="stylesheet" href="css/linearicons.css">
-	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<!-- <link rel="stylesheet" href="css/font-awesome.min.css"> -->
 	<link rel="stylesheet" href="css/themify-icons.css">
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/owl.carousel.css">
@@ -53,7 +53,7 @@
 
 	<!--css necesarios para cambiar btn-login-->
 	<link rel="stylesheet" href="css/loginNavbar.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
@@ -348,7 +348,7 @@
 				<div class="col-lg-3 col-md-6">
 					<div class="single-team-item">
 						<div class="team-img">
-							<img src="assets/webfonts/Sofia.jpg" alt="Sofía Urrutia" class="img-fluid">
+							<img src="" alt="Sofía Urrutia" class="img-fluid">
 						</div>
 						<h4>Sofía Urrutia <span>Diseñadora Principal</span></h4>
 						<ul class="social-link-team">
@@ -376,7 +376,7 @@
 				<div class="col-lg-3 col-md-6">
 					<div class="single-team-item">
 						<div class="team-img">
-							<img src="assets/webfonts/Adrian.jpg" alt="Adrian Mendez" class="img-fluid">
+							<img src="" alt="Adrian Mendez" class="img-fluid">
 						</div>
 						<h4>Adrian Mendez <span>Vendedor</span></h4>
 						<ul class="social-link-team">
@@ -447,19 +447,19 @@
 				<div class="col-lg-12">
 					<div class="logo-carousel-inner">
 						<div class="single-logo-item">
-							<img src="assets/img/company-logos/1.png" alt="">
+							<img src="" alt="">
 						</div>
 						<div class="single-logo-item">
-							<img src="assets/img/company-logos/2.png" alt="">
+							<img src="" alt="">
 						</div>
 						<div class="single-logo-item">
-							<img src="assets/img/company-logos/3.png" alt="">
+							<img src="" alt="">
 						</div>
 						<div class="single-logo-item">
-							<img src="assets/img/company-logos/4.png" alt="">
+							<img src="" alt="">
 						</div>
 						<div class="single-logo-item">
-							<img src="assets/img/company-logos/5.png" alt="">
+							<img src="" alt="">
 						</div>
 					</div>
 				</div>
